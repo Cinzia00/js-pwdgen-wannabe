84ab4f261eaeeaaa593ee704aee5712d3bba3f0c
Cinzia Pulvirenti <cinzia2092@hotmail.it>
2023-01-30 16:37:59 +0100

    Fix sistemate variabili

--- app.js
+++ app.js
@@ -2,26 +2,25 @@ document.getElementById('nome-utente').innerHTML = 'Buona sera';
 
 
 //Chiedere nome utente
-prompt('Inserisci il tuo nome');
 let name = prompt('Inserisci il tuo nome');
+console.log(name)
 
 
 //Chiedere il suo cognome,
-prompt('Inserisci il tuo cognome');
 let lastName = prompt('Inserisci il tuo cognome');
-
+console.log(lastName)
 
 //Chiedere il suo colore preferito
-prompt('Inserisci il tuo colore preferito');
 let color = prompt('Inserisci il tuo colore preferito');
+console.log(color)
 
 //Chiedere un numero
-prompt('Inserisci un numero');
 let number = prompt('Inserisci un numero');
+console.log(number)
 
 
 //Risultato della concatenazione di: <nome><cognome><colorePreferito><numero></numero>
 
 let profiloUtente = name + lastName + color + parseFloat(number);
 
-console.log('profiloUtente')
+console.log(profiloUtente)
